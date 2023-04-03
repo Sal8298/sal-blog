962672eb0fbc118c33c8f62aacf685dd58abc75d
Sal8298 <salahuddinasim95@gmail.com>
2023-04-02 22:38:55 -0500

    Fleshed out config file.

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        //Env file can store local data.
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            // Decides which port localhost connected to.
+            host: 'localhost',
+            dialect: 'mysql',
+            dialectOptions: {
+            decimalNumbers: true,
+            },
+            port: 3306,
+        }
+    );
+}
+
+module.exports = sequelize;
