30d82a5923082f772657cbd3324cc715998fd9db
Sal8298 <salahuddinasim95@gmail.com>
2023-04-06 14:33:19 -0500

    Removed non-working code.

--- models/index.js
+++ models/index.js
@@ -2,29 +2,4 @@ const User = require('./User');
 const Comment = require('./Comment');
 const Post = require('./Post');
 
-User.hasMany(Post, {
-    foreignKey: 'user_id',
-});
-Post.belongsTo(User, {
-    foreignKey: 'post_user_id',
-});
-
-User.hasMany(Comment, {
-    foreignKey: 'comment_user_id',
-});
-
-Comment.hasOne(Post, {
-    foreignKey: 'post_id',
-});
-Post.hasMany(Comment, {
-    foreignKey: 'post_id',
-});
-
-Comment.belongsTo(User, {
-    foreignKey: 'comment_user_id',
-});
-User.hasMany(Comment, {
-    foreignKey: 'comment_user_id',
-});
-
 module.exports = { User, Comment, Post };
